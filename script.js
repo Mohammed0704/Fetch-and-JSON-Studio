55b5dee2ecefbf88fa41540f7704bce20070ee6a
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-07-24 19:58:34 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    const container = document.getElementById("container");
+
+    async function getAstronauts() {
+        let response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+
+        let data = await response.json();
+
+        console.log(data);
+    }
+
+
+});
