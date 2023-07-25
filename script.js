3db12b93be2583480fd963eef5e0730a89c7b0c1
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-07-24 20:16:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ window.addEventListener("load", function() {
         let data = await response.json();
 
         console.log(data);
+
+        for (let i=0; i<data.length; i++){
+            container.innerHTML = `
+            <div class="astronaut">
+            <div class="bio">
+                <h3>${data[i].firstName + data[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${data[i].hoursInSpace}</li>
+                    <li>Active: ${data[i].active}</li>
+                    <li>Skills: ${data[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src=${data[i].picture}>
+        </div>
+        `
+        }
+
     }
 
     getAstronauts();
